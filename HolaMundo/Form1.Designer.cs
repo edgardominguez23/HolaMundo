fc27b574a4a3601397fcd38c0094c825320270d1
Edgar Dominguez <edgar.dominguez5434@alumnos.udg.mx>
2023-10-01 17:45:56 -0600

    Agregar archivos de proyecto.

---
+++ HolaMundo/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace HolaMundo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtContrasenaUno = new System.Windows.Forms.TextBox();
+            this.txtContrasenaDos = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtContrasenaUno
+            // 
+            this.txtContrasenaUno.Location = new System.Drawing.Point(252, 56);
+            this.txtContrasenaUno.Name = "txtContrasenaUno";
+            this.txtContrasenaUno.Size = new System.Drawing.Size(358, 26);
+            this.txtContrasenaUno.TabIndex = 0;
+            // 
+            // txtContrasenaDos
+            // 
+            this.txtContrasenaDos.Location = new System.Drawing.Point(252, 110);
+            this.txtContrasenaDos.Name = "txtContrasenaDos";
+            this.txtContrasenaDos.Size = new System.Drawing.Size(358, 26);
+            this.txtContrasenaDos.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ingresa una contraseña";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Repite contraseña";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Location = new System.Drawing.Point(426, 291);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(145, 40);
+            this.btnValidar.TabIndex = 4;
+            this.btnValidar.Text = "Validar";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            this.btnValidar.Click += new System.EventHandler(this.btnValidar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnValidar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtContrasenaDos);
+            this.Controls.Add(this.txtContrasenaUno);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtContrasenaUno;
+        private System.Windows.Forms.TextBox txtContrasenaDos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnValidar;
+    }
+}
+
---
+++ HolaMundo/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HolaMundo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+            Regex validacion = new Regex("^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[\\W_]).*$");
+
+            if (string.IsNullOrEmpty(txtContrasenaUno.Text) && 
+               string.IsNullOrEmpty(txtContrasenaDos.Text))
+            {
+                MessageBox.Show("Los campos estan vacios");
+                return;
+            }
+
+            if (!txtContrasenaUno.Text.Equals(txtContrasenaDos.Text))
+            {
+                MessageBox.Show("Las contraseñas no son iguales");
+                return;
+            }
+
+            if (validacion.IsMatch(txtContrasenaUno.Text))
+            {
+                MessageBox.Show("La contraseña es valida");
+                return;
+            }
+            else
+            {
+                MessageBox.Show("La contraseña no es valida");
+                return;
+            }
+        }
+    }
+}
